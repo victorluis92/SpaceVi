41fc0bff7751649507d554634dd6c80ede4ebed8
unknown <victorluishernandez92@gmail.com>
2022-09-24 12:16:04 -0500

    se agrego una segunda pantalla

--- app/src/main/java/com/example/spacevi/MainActivity.kt
+++ app/src/main/java/com/example/spacevi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.spacevi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,37 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnGuardar:Button=findViewById(R.id.btnGuardar)
 
-        btnGuardar.setOnClickListener{
-
-            val campoNombre:EditText=findViewById(R.id.txtID)
-            val campoApellido:EditText=findViewById(R.id.txtNombre)
-            val campoTelefono:EditText=findViewById(R.id.txtTelefono)
-
-            val nombre:String=campoNombre.text.toString()
-            val apellido:String=campoApellido.text.toString()
-            val telefono:Int=campoTelefono.text.toString().toInt()
-
-
-            saveFirestore(nombre,apellido,telefono)
+        val btnQuieroDarARentar:Button=findViewById(R.id.btnQuieroDarARentar)
+        btnQuieroDarARentar.setOnClickListener{
+            val lanzar=Intent(this,PantallaDatosarrendador::class.java)
+            startActivity(lanzar)
         }
-    }
-    fun saveFirestore(nombre:String, apellido:String, telefono: Int){
-        val db=FirebaseFirestore.getInstance()
-        val user:MutableMap<String,Any> =HashMap()
-        user["nombre"]=nombre
-        user["apellido"]=apellido
-        user["telefono"]=telefono
 
-        db.collection("usuarios")
-            .add(user)
-            .addOnSuccessListener{
-                Toast.makeText(this@MainActivity, "exito", Toast.LENGTH_LONG).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(this@MainActivity, "no hay exito", Toast.LENGTH_LONG).show()
 
-            }
     }
 }
